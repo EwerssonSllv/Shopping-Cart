6a82c5d2242c6ee22f613fdcdaadcb2eaad82c2d
EwerssonSllv <ewerssonsllv@gmail.com>
2026-01-11 11:44:34 -0300

    Context API Added

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,33 @@
+import { useState, createContext, type ReactNode } from "react";
+
+interface CartContextData {
+    cart: CartProps[]
+}
+
+interface CartProps {
+    id: number,
+    title: string,
+    description: string,
+    price: number,
+    cover: string,
+    amount: number,
+    total: number
+}
+
+interface CartProviderProps {
+    children: ReactNode
+}
+
+export const CartContext = createContext({} as CartContextData)
+
+function CartProvider({ children }: CartProviderProps) {
+    const [cart, setCart] = useState<CartProps[]>([])
+
+    return (
+        <CartContext.Provider value={{ cart }}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export default CartProvider
